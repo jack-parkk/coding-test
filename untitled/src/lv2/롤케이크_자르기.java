43b40d142487d490db20463cf6b6625632ace10e
jyp <jooyoung.park@lots-on.co.kr>
2026-06-25 22:02:34 +0900

    추가 : 케이크

--- untitled/src/lv2/롤케이크_자르기.java
+++ untitled/src/lv2/롤케이크_자르기.java
@@ -2,35 +2,41 @@ package lv2;
 import java.util.*;
 public class 롤케이크_자르기 {
     public static void main(String[] args) {
-        int[] topping = {1, 2, 3, 1, 4};
-        int answer = 0;
-        // 가짓수 몇개인지 확인하기
-        Set<Integer> cnt = new HashSet<>();
-        for (int i : topping) {
-            cnt.add(i);
+        class Solution {
+            public int solution(int[] topping) {
+                int answer = 0;
+
+                Set<Integer> left = new HashSet<>();
+                Map<Integer, Integer> right = new HashMap<>();
+
+                // 처음에는 오른쪽에 전체 토핑이 다 있다고 생각
+                for (int t : topping) {
+                    right.put(t, right.getOrDefault(t, 0) + 1);
+                }
+
+                // 하나씩 오른쪽에서 왼쪽으로 옮긴다
+                for (int i = 0; i < topping.length - 1; i++) {
+                    int t = topping[i];
+
+                    // 왼쪽에 추가
+                    left.add(t);
+
+                    // 오른쪽에서 하나 제거
+                    right.put(t, right.get(t) - 1);
+
+                    // 오른쪽에 해당 토핑이 더 이상 없으면 종류에서 제거
+                    if (right.get(t) == 0) {
+                        right.remove(t);
+                    }
+
+                    // 왼쪽 종류 수와 오른쪽 종류 수가 같으면 공평
+                    if (left.size() == right.size()) {
+                        answer++;
+                    }
+                }
+
+                return answer;
+            }
         }
-        int num = cnt.size(); // 토핑 가짓수
-        System.out.println("토핑 수 : " + cnt);
-
-        //만약 공평하게 나누지 못한다면 0 반환
-        // 조건 1 : 가짓 수가 짝수가 아닐경우
-        if (num % 2 == 1) {
-            System.out.println(answer);
-//          return answer;
-        }
-        // 조건 2 : 가짓 수의 갯수가 2 이상이 아닐경우
-
-
-        // 배열을 for으로 돌면서 num/2가 되는 지점부터 num/2보다 커지는 지점까지 answer 갯수 카운트
-        int save = 0;
-        Set<Integer> count = new HashSet<>();
-        for(int i =0; i<topping.length; i++ ) {
-            count.add(topping[i]);
-            save = count.size();
-            if(save == (num/2)) answer++; // 공평하게 나눠진 다면 answer 가짓수 추가
-            if (save > (num/2)) break; // 공평함을 벗어난다면 루프문 벗어남
-        }
-
-        System.out.println(answer);
     }
 }
